2a7e789ed03653df2ad6c07f8d390d5953759e91
Szymon Rykała <szymonrykala@gmail.com>
2026-03-06 22:41:40 +0100

    code formatting

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter} from 'react-router'
+import { HashRouter } from 'react-router'
 import { Toaster } from 'react-hot-toast';
 
 
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
       <Toaster />
-      <App/>
+      <App />
     </HashRouter>
   </StrictMode>,
 )
